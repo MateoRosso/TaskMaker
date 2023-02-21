5213a05595f8199d4e46394c253ca20ef9c9f1d2
MateoRosso <rossomateoe9@gmail.com>
2023-02-21 13:07:25 -0300

    first commit

---
+++ src/components/taskList.jsx
@@ -0,0 +1,26 @@
+import TaskCard from "./TaskCard";
+import { useContext } from "react";
+import {TaskContext} from "../context/TaskContext"
+
+function taskList() {
+
+const {tasks} = useContext(TaskContext)
+
+if (tasks.length == 0) return <h1 className="text-white text-4xl font-bold text-center">No hay tareas aun</h1>;
+
+
+  return (
+    <div className="grid grid-cols-4 gap-2">
+      {tasks.map((task, id) => (
+        <TaskCard key={id} task={task}/>
+      ))}
+    </div>
+  );
+
+}
+
+
+
+export default taskList;
+
+
